f16ecdcb6c5e7b16cb27f11889f681d5ae8ee3cf
Ian Agpawa <agpawaji@gmail.com>
2019-03-02 15:48:44 -0500

    feat: Update to project

---
+++ FileObject.py
@@ -0,0 +1,7 @@
+class FileObject:
+    def __init__(self, positions_arr):
+        self.positions_arr = positions_arr
+
+    def myFunc():
+        return 'Hello'
+
---
+++ execute.py
@@ -0,0 +1,4 @@
+import sys
+from FileObject import FileObject
+
+print(FileObject.myFunc())
