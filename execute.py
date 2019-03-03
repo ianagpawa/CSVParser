a307ee51f9fffa5a8b8c2829566cf1e638650f56
Ian Agpawa <agpawaji@gmail.com>
2019-03-02 19:02:23 -0500

    refactor: Refactor execute

--- execute.py
+++ execute.py
@@ -2,13 +2,11 @@ import sys
 from Utils import Utils
 
 def execute():
+    """
+    Parses csv file by section, creates individual csv for each section.
+    """
     utils = Utils(sys.argv[1], sys.argv[2])
-    if sys.argv[3] == "base":
-        utils.create_base_files()
-        return
-    if  '.txt' in sys.argv[3]:
-        utils.update_action_files(sys.argv[3])
-        return
+    utils.create_updated_action_files(sys.argv[3]) if len(sys.argv) > 3 else utils.create_base_files()
 
 
 execute()
